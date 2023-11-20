c15a785a2d785c40a7b9f26d43a17bc7dafc2daa
Jalinsy <jiaji200324@gmail.com>
2023-11-20 22:04:47 +0900

    test

---
+++ ChangeChar.java
@@ -0,0 +1,16 @@
+public class ChangeChar {
+	public static void main(String[] args) {
+//		\t : 一个制表位，实现对齐的功能
+		System.out.println("东京\t横滨\t神奈川");
+//		\n : 换行符
+		System.out.println("jack\nsmith\nmary");
+//		\\ : 一个\
+		System.out.println("C:\\Windows\\System32\\cmd.exe");
+//		\" : 一个"
+		System.out.println("社长说:\"好好学Java\"");
+//		\' : 一个"
+		System.out.println("社长说:\'好好学Java\'");
+//		\r : 一个回车
+		System.out.println("东京\r横滨");
+	}
+}
